e95d1c0e10897a18d32834fc1de2d480df93b6cf
Joe <joeacarstairs@gmail.com>
2021-04-18 13:57:52 +0100

    Implements template check for whether a given type is a comparison on two other given types.

---
+++ is_comparison.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <type_traits>
+
+template <typename _Compare, typename _Left, typename _Right = _Left>
+class is_comparison
+{
+    template<typename __Compare, typename __Left, typename __Right>
+    static auto test(int)
+        -> decltype(std::invoke(std::declval<__Compare>(), std::declval<__Left>(), std::declval<__Right>()) ? 0 : 0, std::true_type());
+
+    template<typename, typename, typename>
+    static auto test(...)->std::false_type;
+
+public:
+    static const bool value = decltype(test<_Compare, _Left, _Right>(0))::value;
+};
+
+template <typename _Compare, typename _Left, typename _Right = _Left>
+inline constexpr bool is_comparison_v = is_comparison<_Compare, _Left, _Right>::value;
