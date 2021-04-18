a13793b564d25bef6bd2ce5ea5be7a69cf6ad80b
Joe <joeacarstairs@gmail.com>
2021-04-18 18:34:44 +0100

    Object lists can now be sorted by comparisons on members.

--- object_list.h
+++ object_list.h
@@ -10,6 +10,7 @@
 #include "is_equatable.h"
 #include "is_predicate.h"
 #include "is_comparison.h"
+#include "is_member_of.h"
 
 
 namespace easylist
@@ -157,12 +158,45 @@ namespace easylist
         template <typename _Compare, std::enable_if_t<is_comparison_v<_Compare, _Type>, bool> = true>
         void sort(_Compare comparer)
         {
-            std::sort(_Mybase::begin(), _Mybase::end(), cast_static_comparison<_Compare, _Type>(comparer));
+            std::sort(this->begin(), this->end(), cast_static_comparison<_Compare, _Type>(comparer));
         }
 
         void sort()
         {
             this->sort(std::less<>{});
+        }
+
+        template <
+            typename _Compare,
+            typename _Callable,
+            typename... _Args,
+            typename _Result = std::remove_reference_t<decltype(std::invoke(std::declval<_Callable>(), std::declval<_Type>(), std::declval<_Args>()...))>,
+            std::enable_if_t<
+                std::conjunction_v<
+                    is_comparison<_Compare, _Result>,
+                    is_const_member_of<_Callable, _Type, _Args...>
+                >,
+                bool
+            > = true
+        >
+        void sort(_Compare comparer, _Callable member, const _Args&... args)
+        {
+            static auto static_comparer = cast_static_comparison<_Compare, _Type>(comparer);
+            static auto pred = [comparer, member, args...](const _Type& lhs, const _Type& rhs) -> auto {
+                const _Result resultLhs = std::invoke(member, lhs, args...);
+                const _Result resultRhs = std::invoke(member, rhs, args...);
+                return static_comparer(resultLhs, resultRhs);
+            };
+            std::sort(this->begin(), this->end(), pred);
+        }
+
+        template <
+            typename _Callable,
+            typename... _Args
+        >
+        void sort(_Callable member, const _Args&... args)
+        {
+            this->sort(std::less<>{}, member, args...);
         }
     };
 }
--- tester.cpp
+++ tester.cpp
@@ -11,7 +11,8 @@ class C
 public:
     int n;
     C(int n) { this->n = n; }
-    int get() { return n; }
+    int get() const { return n; }
+    int getDiff(int other) const { return (n > other ? n - other : other - n); }
     operator const std::string() const { return "I am number " + std::to_string(n); }
     friend std::ostream& operator<<(std::ostream& stream, const C& c) { return stream << (std::string)(c); }
     bool operator==(const C& other) const { return n == other.n; }
@@ -25,23 +26,18 @@ bool evener(const C& c1, const C& c2) { return (c1.n % 2) && !(c2.n % 2); }
 
 int main()
 {
-    C c = C(1);
+    const C c = C(1);
+    c.getDiff(0);
 
-    object_list<C> cList = object_list<C>({ c, C(2), C(3) });
+    object_list<C> cList = object_list<C>({ C(0), C(2), C(-3) });
 
-    cList.sort();
+    cList.sort(std::less<>{}, & C::n);
     std::cout << cList << std::endl;
 
-    cList.sort(&C::operator>);
+    cList.sort(std::less<>{}, & C::getDiff, 0);
     std::cout << cList << std::endl;
 
-    cList.sort(&C::operator<);
-    std::cout << cList << std::endl;
-
-    cList.sort(&odder);
-    std::cout << cList << std::endl;
-
-    cList.sort(&evener);
+    cList.sort(& C::get);
     std::cout << cList << std::endl;
         
     return 0;
