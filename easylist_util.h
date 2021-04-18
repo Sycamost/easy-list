1991615dd25c8a7549ed02dad25fc286c742921a
Joe <joeacarstairs@gmail.com>
2021-04-18 09:08:26 +0100

    Re-organises into a more transparent, factored file structure.

---
+++ easylist_util.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define EASYLIST_TEMPLATE_OBJ_LIST_DEFN template<class _Type, class _Alloc = std::allocator<_Type>>
+#define EASYLIST_TEMPLATE_OBJ_LIST_DECL template<class _Type, class _Alloc>
